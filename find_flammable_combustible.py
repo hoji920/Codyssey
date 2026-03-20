c184064a9e00a30622de7332d6edab8628873b97
hoji920 <92128530@naver.com>
2026-03-20 14:58:49 +0900

    feat : 리스트 할당 및 인화성 높은 순으로 정렬

--- find_flammable_combustible.py
+++ find_flammable_combustible.py
@@ -1,6 +1,31 @@
 import csv
 
-with open('Mars_Base_Inventory_List.csv','r',encoding='utf-8') as file:
+file_path = 'Mars_Base_Inventory_List.csv'
+
+with open(file_path,'r',encoding='utf-8') as file:
+
+    # Mars_Base_Inventory_List 출력
+    print(f"\n----- {file_path} 출력 -----")
+    for files in file:
+        print(files.strip())
+
+    # 파일 포인터를 다시 맨 처음(0)으로 이동
+    file.seek(0)
+
+    # 리스트로 변환 및 배열 할당
     reader = csv.reader(file)
+    list =[]
     for readers in reader:
-        print(readers)
+        # print(readers)
+        list.append(readers)
+
+    # 인화성이 높은 순으로 정렬
+    list.sort(key=lambda x: x[4],reverse=True)
+    print(f"\n----- {file_path} 오름차순 정렬 -----")
+    for sortList in list:
+        print(sortList)
+
+    # 인화성 지수 0.7이상 출력
+    print(f"\n----- {file_path} 인화성 지수가 0.7 이상 출력 -----")
+
+    
